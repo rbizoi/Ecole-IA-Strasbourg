822941d34f672b1d32fb200fed82a0b24e07934f
rbizoi <razvan.bizoi@gmail.com>
2020-09-30 11:32:34 +0200

    Create Atelier_5.2.2.py

---
+++ Ateliers_Python_Syntaxe/Atelier_5.2.2.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# Atelier 5.2.2
+
+lst = ['Jean-Michel','Marc','Vanessa','Anne','Maximilien','Alexandre-Benoît','Louise']
+for e in lst:
+    print("%s : %s caractères" % (e, len(e)))
